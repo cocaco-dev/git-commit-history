24ac08bc7aa4419fb88ade5d4d2b39d2ccd207e7
jose carlos barriga <jose.c.barriga@gmail.com>
2022-08-03 15:01:35 -0400

    [add] molecules

---
+++ client/src/components/molecules/Card.jsx
@@ -0,0 +1,32 @@
+// custom functional component for input
+import React from 'react';
+
+
+
+const Card = ({ commitInfo}) => {
+    return (
+        <div className=" w-full  my-2">
+        <div className="border-r border-b border-l border-t border-gray-400 lg:border-l lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
+          <div className="mb-8">
+            <p className="text-sm text-gray-600 flex items-center">
+              <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z" />
+              </svg> sha: {commitInfo.sha}
+            </p>
+            <div className="text-gray-900 font-bold text-l mb-1">Commit:</div>
+            <p className="text-gray-700 text-base">{commitInfo.message}</p>
+          </div>
+          <div className="flex items-center">
+            <img className="w-10 h-10 rounded-full mr-4" src={commitInfo.avatar} alt="Avatar"/>
+            <div className="text-sm">
+              <p className="text-gray-900 leading-none"><span className='font-bold'>{commitInfo.author}</span> ({commitInfo.email})</p>
+              <p className="text-gray-600">{commitInfo.date && new Date(commitInfo.date).toTimeString()}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Card;
+
